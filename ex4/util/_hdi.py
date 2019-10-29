591c4001b94108c86f291ce6d1b566aeeeecc958
Kim Albertsson <ketost@gmail.com>
2019-10-29 13:03:01 +0100

    ex4: Add documentation for hdi

--- ex4/util/_hdi.py
+++ ex4/util/_hdi.py
@@ -2,6 +2,35 @@
 import numpy as np
 
 def hdi(sample, cred_mass=0.95, *args, method='std', **kwargs):
+    '''Find an estimate of the highest density interval
+
+    Arguments
+    ---------
+    sample : array-like
+        Samples from the distribution to approximate hdi for. Should be one
+        dimensional due to technical limitations.
+    cred_mass : float
+        Proportion of probability mass that should be included in HDI.
+        Determines length of resulting interval.
+
+    Keyword arguments
+    -----------------
+    method : str, either ['std' or 'hist']
+        The method used to calculate the hdi.
+
+        When `method=='std'`: Uses the implementation of J. K. Kruschke in
+        "Doing Bayesian Data Analysis" (Section 25.2.4).
+
+        When `method=='hist'`: Uses a histogram method to iteratively find the
+        highest density bins and appoximate the interval(s) through bin edges.
+        Theoretically supports multiple disconnected intervals, but this is
+        untested.
+
+    Returns
+    -------
+    tuple (hdi_min, hdi_max)
+    '''
+
     assert(method in ['std', 'hist'])
     if method == 'std':
         return hdi_std(sample, cred_mass,*args, **kwargs)
@@ -10,13 +39,44 @@ def hdi(sample, cred_mass=0.95, *args, method='std', **kwargs):
         return hdi_hist(h, x, *args, **kwargs)
 
 def hdi_std(sample, cred_mass=0.95):
+    '''Find an estimate of the highest density interval
+
+    Estimates the highest density interval (HDI) of a given sample by relying
+    on the equivalence of the HDI and the shortest contiguous interval
+    containing a given mass. This holds only for unimodal distributions.
+
+    The idea is to check each contiguous groping of the sought after number of
+    samples and find the grouping that spans the shortest interval
+
+    This can be done in linear time for univariate distributions if the samples
+    are sorted (yielding n log n overall).
+
+    Arguments
+    ---------
+    sample : array-like
+        Samples from the distribution to approximate hdi for. Should be one
+        dimensional due to technical limitations.
+    cred_mass : float
+        Proportion of probability mass that should be included in HDI.
+        Determines length of resulting interval.
+
+    Returns
+    -------
+    tuple (hdi_min, hdi_max)
+    '''
+
     x = sorted(sample)
+
+    # Length of shortest interval (in samples) containing given mass.
     ciIdxInc = int(np.ceil(cred_mass*len(sample)))
+
+    # Calculate the resulting spans of the groups
     nCIs = len(sample) - ciIdxInc
     ciWidth = np.zeros(nCIs)
     for i in range(nCIs):
         ciWidth[i] = x[i+ciIdxInc] - x[i]
 
+    # HDI is the shortest interval
     hdi_min = x[np.argmin(ciWidth)]
     hdi_max = x[np.argmax(ciWidth) + ciIdxInc]
 
@@ -24,10 +84,38 @@ def hdi_std(sample, cred_mass=0.95):
 
 
 def hdi_hist(h, x, cred_mass=0.95):
-    # convert to probability mass
-    # h = h*(x[1]-x[0])/np.sum(h)
+    '''Find an estimate of the highest density interval
+
+    Estimates the highest density interval (HDI) of a given sample by
+    iteratively finding the histogram bin with the highest density util the
+    sought after probability mass is found.
+
+    Technically supports non-unimodal distributions, but this is untested.
+
+    Note: Use ´util.hdi(sample, cred_mass, method='hist')` for an interface
+    that accepts a sample and not a pre-binned histogram.
+
+    Arguments
+    ---------
+    h : array-like, shape (N,)
+        A histogram of a sample, such as the one produced by
+        `h, x = np.histogram(sample)`.
+    x : array-like, shape (N+1,)
+        The bin edges corresponding to `h`, such as those produced by
+        `h, x = np.histogram(sample)`.
+
+    Returns
+    -------
+    tuple (hdi_min, hdi_max), or
+    list [(hdi_min0, hdi_max0), (hdi_min1, hdi_max1), ...]
+        If the procedure returns only a single interval this is output.
+        Otherwise a list of all intervals are output.
+    '''
+
+    # Convert to probability mass
     h = h/np.sum(h)
 
+    # Find the highest mass bins
     integrated_mass = 0
     bins = []
     while integrated_mass <= cred_mass:
@@ -36,6 +124,7 @@ def hdi_hist(h, x, cred_mass=0.95):
         bins += [i]
         h[i] = 0
 
+    # Merge contiguous bins to a single interval
     limits = []
     current_limit = [0, 0]
     bins = sorted(bins)
